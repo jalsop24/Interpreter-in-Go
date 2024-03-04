32d20c11e96b0fefa681cbdcd004c1f97814906e
James <j.alsop99@hotmail.com>
2024-03-04 22:37:28 +0000

    Updated repl to use evaluator

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"go_interpreter/monkey/evaluator"
 	"go_interpreter/monkey/lexer"
 	"go_interpreter/monkey/parser"
 	"io"
@@ -43,8 +44,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
